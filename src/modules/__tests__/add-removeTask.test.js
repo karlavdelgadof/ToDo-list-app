8d3d3d99f21f478a99bf5c325c6e8859422d30a1
karlavdelgadof <karlavdelgadof@gmail.com>
2022-07-14 12:02:32 -0500

    Fix linter errors

--- src/modules/__tests__/add-removeTask.test.js
+++ src/modules/__tests__/add-removeTask.test.js
@@ -1,36 +1,34 @@
 /** * @jest-environment jsdom */
-import UIDisplay from "../userInterface.js";
-import removeTask from "../removeTask.js";
+import UIDisplay from '../userInterface.js';
+import removeTask from '../removeTask.js';
 
-describe("Add and Remove Task", () => {
-  test("Add a new task", () => {
+describe('Add and Remove Task', () => {
+  test('Add a new task', () => {
     document.body.innerHTML = '<ul id="task-list"></ul>';
 
     const newTask = {
-      description: "desc",
+      description: 'desc',
       completed: false,
       index: 1,
     };
 
     UIDisplay.createTask(newTask);
-    
-    const list = document.querySelectorAll(".task");
+
+    const list = document.querySelectorAll('.task');
     expect(list).toHaveLength(1);
   });
 
-  test("Remove a task", () => {
-    document.body.innerHTML =
-      '<ul id="task-list"><li class="task"><input type="checkbox" class="checkB"><input class="task-d"><i class="fa-solid fa-ellipsis-vertical show"></i><i class="fa-solid fa-trash-can"></i></li></ul>';
-  
+  test('Remove a task', () => {
+    document.body.innerHTML = '<ul id="task-list"><li class="task"><input type="checkbox" class="checkB"><input class="task-d"><i class="fa-solid fa-ellipsis-vertical show"></i><i class="fa-solid fa-trash-can"></i></li></ul>';
+
     const newTask = {
-      description: "Dummie",
+      description: 'Dummie',
       completed: true,
       index: 1,
     };
     removeTask(newTask);
-  
-    const list = document.querySelectorAll(".task");
+
+    const list = document.querySelectorAll('.task');
     expect(list).toHaveLength(0);
   });
 });
-
